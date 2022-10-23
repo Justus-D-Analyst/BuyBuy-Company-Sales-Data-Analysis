f0b441b637629cf53995f434ee4d8c5b0dd35fe7
Justus Oguamanam <88462627+Justus-D-Analyst@users.noreply.github.com>
2022-10-23 13:02:45 +0100

    Update BuyBuyScript.sql

--- BuyBuyScript.sql
+++ BuyBuyScript.sql
@@ -1,4 +1,7 @@
+---to create Database
 CREATE DATABASE Buybuy_company;
+
+---to create Schema and Table
 create schema buybuysalesdata
 create table buybuysalesdata.sales
 (Sales_date date,
@@ -21,71 +24,63 @@ create table buybuysalesdata.sales
     Revenue int,
  	Profit int
     )
-	
+
+---to view all table
 select * from buybuysalesdata.sales
 
 
 
 --------------Time-Profit Analysis------------------
 
------------------1a---------------------
+---query that returns the total profit made by BuyBuy Company from 1Q11 to 4Q16 (all quarters of every year)
 select quarter_of_the_year,sum(profit) as total_profit from buybuysalesdata.sales
 group by quarter_of_the_year
 order by 1
 
------------------1b------------------
+---queries that return the total profit made by BuyBuy Company in Q2 of every year from 2011 to 2016.
 select quarter_of_the_year,sales_year,sum(profit) as total_profit from buybuysalesdata.sales
 where quarter_of_the_year = 'Q2'
 group by sales_year,quarter_of_the_year
 order by 2
 
------------------1c------------------
+---query that returns the annual profit made by BuyBuy Company from the year 2011 to 2016.
 select sales_year,sum(profit) as total_profit from buybuysalesdata.sales
 group by sales_year
 order by 1
 
------------------1d-------------------
----Create a chart to visualize 1b
+--------------Regional-Profit Analysis------------------
 
-
------------------2a-----------------
+---queries that return the countries where BuyBuy has made the most profit and also the least profit of all-time. Your query must display both results on the same output.
 select customer_country, max(profit) as max_profit, min(profit) as min_profit from buybuysalesdata.sales
 group by customer_country
 
-----------------2b------------------
+---query that shows the Top-10 most profitable countries for BuyBuy Company sales operations from 2011 to 2016
 select customer_country, sum(profit) as most_profitable from buybuysalesdata.sales
 group by customer_country
 order by 2 desc
 limit 3
 
-----------------2c------------------
+---query that shows the all-time Top-10 least profitable countries for BuyBuy Company sales operations.
 select customer_country, sum(profit) as most_profitable from buybuysalesdata.sales
 group by customer_country
 order by 2
 limit 3
 
-----------------2d----------------
---Create a chart to visualize 2b
+--------------Product-Revenue Analysis------------------
 
-
-----------------3a----------------
+---query that ranks all product categories sold by Buybuy Company, from least amount to the most amount of all-time revenue generated.
 select product_category, sum(revenue) as prodcat_rank from buybuysalesdata.sales
 group by product_category
 order by 2 
 
-----------------3b-----------------
+---query that returns Top-2 product categories offered by Buybuy Company with an all-time high number of units sold.
 select product_category, sum(unit_sold) as total_unit_sold from buybuysalesdata.sales
 group by product_category
 order by 2
 limit 2
 
-----------------3c-----------------
+---query that shows the Top 10 highest-grossing products sold by BuyBuy Company based on all-time profits.
 select product, sum(profit) as prod_profit from buybuysalesdata.sales
 group by product
 order by 2 desc
 limit 10
-
-----------------3d------------------
----Create a chart to visualize 3b
-
-
